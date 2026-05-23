00f43fbc267cb614d440850f32e2f7f4a7f847ff
yissegnonherve <yissegnonherve@gmail.com>
2026-05-23 11:11:43 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,12 @@
 <?php
-// Vos identifiants de connexion InfinityFree visibles sur votre photo
+// Vos identifiants réels InfinityFree
 $host     = "sql306.infinityfree.com"; 
 $port     = "3306"; 
 $dbname   = "if0_41986212_ile_multiservices"; 
 $username = "if0_41986212"; 
-$password = "nWdLBOTvfRn"; // Cliquez sur le petit œil sur votre photo pour le copier
+$password = "nWdLBOTvfRn"; // Enlevez ceci et mettez votre mot de passe secret à la place
 
 try {
-    // Connexion sécurisée avec PDO
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
