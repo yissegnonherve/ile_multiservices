250ee1443e4d934d77831dad9c8156b73763f8e3
yissegnonherve <yissegnonherve@gmail.com>
2026-05-23 11:09:15 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,17 +1,15 @@
 <?php
-// Vos nouveaux identifiants en ligne (ex: Aiven, Railway, ou autre)
-$host     = "votre_hote_de_base_de_donnees.com"; 
-$port     = "3306"; // Le port fourni par votre hébergeur de BDD
-$dbname   = "votre_nom_de_base_de_donnees";
-$username = "votre_utilisateur";
-$password = "votre_mot_de_passe";
+// Vos identifiants de connexion InfinityFree visibles sur votre photo
+$host     = "sql306.infinityfree.com"; 
+$port     = "3306"; 
+$dbname   = "if0_41986212_ile_multiservices"; 
+$username = "if0_41986212"; 
+$password = "nWdLBOTvfRn"; // Cliquez sur le petit œil sur votre photo pour le copier
 
 try {
     // Connexion sécurisée avec PDO
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    // Si vous arrivez ici, c'est que la connexion fonctionne !
 } catch(PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 }
